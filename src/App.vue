81656b2687cab58b298db19d69a9c23d4a03f3a3
Angel Arcoraci <angel.arcoraci@gmail.com>
2019-11-29 11:27:38 -0300

    small improvements

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <parallax />
+    <parallax :layers="layers" :width="'900px'" :height="'650px'" />
   </div>
 </template>
 
@@ -10,6 +10,48 @@ export default {
   name: 'app',
   components: {
     Parallax
+  },
+  data() {
+    return {
+      layers: [
+        {
+          name: "bg_sky",
+          horizontalDisplacement: 0.01,
+          verticalDisplacement: 0.01,
+          image: require('@/assets/layer_sky.png')
+        },
+        {
+          name: "bg_clouds",
+          horizontalDisplacement: 0.1,
+          verticalDisplacement: 0.1,
+          image: require('@/assets/layer_clouds.png')
+        },
+        {
+          name: "bg_mountain",
+          horizontalDisplacement: 0.075,
+          verticalDisplacement: 0.075,
+          image: require('@/assets/layer_bg_mountains.png')
+        },
+        {
+          name: "bg_pines",
+          horizontalDisplacement: 0.11,
+          verticalDisplacement: 0.11,
+          image: require('@/assets/layer_bg_pines.png')
+        },
+        {
+          name: "ground",
+          horizontalDisplacement: 0.15,
+          verticalDisplacement: 0.15,
+          image: require('@/assets/layer_ground.png')
+        },
+        {
+          name: "pines",
+          horizontalDisplacement: 0.3,
+          verticalDisplacement: 0.3,
+          image: require('@/assets/layer_pines.png')
+        }
+      ]
+    }
   }
 }
 </script>
--- src/components/Parallax.vue
+++ src/components/Parallax.vue
@@ -1,9 +1,20 @@
 <template>
-  <div id="parallax" @mousemove="onMouseMove">
-    <div class="mouse-pos">
-      x: {{ horizontalDistanceFromCenter }} y: {{ verticalDistanceFromCenter }}
-    </div>
-    <div id="target" class="layer"></div>
+  <div id="parallax" :style="{ width: width, height: height }">
+    <div class="mouse-pos">x: {{ mouseX }} y: {{ mouseY }}</div>
+    <div
+      class="layer"
+      v-for="(layer, index) in stageLayers"
+      :id="`layer_${layer.name}`"
+      :key="index"
+      :style="{
+        backgroundImage: 'url(' + layer.img + ')',
+        left: layer.horizontalOffset,
+        right: layer.horizontalOffset,
+        top: layer.verticalOffset,
+        bottom: layer.verticalOffset
+      }"
+      :ref="`layer_${layer.name}`"
+    ></div>
   </div>
 </template>
 
@@ -13,7 +24,21 @@ export default {
     return {
       mouseX: 0,
       mouseY: 0,
-      layers: []
+      stageLayers: []
+    }
+  },
+  props: {
+    layers: {
+      type: Array,
+      required: true
+    },
+    width: {
+      type: String,
+      default: '600px'
+    },
+    height: {
+      type: String,
+      default: '600px'
     }
   },
   methods: {
@@ -21,31 +46,33 @@ export default {
 
       requestAnimationFrame(this.animateStage);
 
+      // values will be relative to the mouse position within the window and its center
       const windowWidth = window.innerWidth;
       const windowHeight = window.innerHeight;
 
-      // x related values
+      // create a relation between how much the mouse has drifted from 0,0 (center of screen)
       const horizontalDistanceFromCenter = Math.abs(this.mouseX - windowWidth / 2);
-      const amountOfHorizontalMovement = horizontalDistanceFromCenter * 0.2 / (windowWidth / 2);
-
-      // y related values
       const verticalDistanceFromCenter = Math.abs(this.mouseY - windowHeight / 2);
-      const amountOfVerticalMovement = verticalDistanceFromCenter * 0.2 / (windowHeight / 2);
 
-      let horizontalMoveDirection = 1;
-      if (this.mouseX - windowWidth / 2 > 0) {
-        horizontalMoveDirection = -1;
+      for (let index = 0; index < this.stageLayers.length; index++) {
+        const layer = this.stageLayers[index];
+        if (!document.getElementById(`layer_${layer.name}`)) {
+          continue;
+        }
+
+        // calculate how much the layer respond to movement based on the displacement value
+        const amountOfHorizontalMovement = horizontalDistanceFromCenter * layer.horizontalDisplacement / (windowWidth / 2);
+        const amountOfVerticalMovement = verticalDistanceFromCenter * layer.verticalDisplacement / (windowHeight / 2);
+
+        // assign a direction opposite to mouse movement
+        const horizontalDirection = this.mouseX - windowWidth / 2 > 0 ? -1 : 1;
+        const verticalDirection = this.mouseY - windowHeight / 2 > 0 ? -1 : 1;
+
+        const xMovement = amountOfHorizontalMovement * horizontalDirection * 100;
+        const yMovement = amountOfVerticalMovement * verticalDirection * 100;
+
+        document.getElementById(`layer_${layer.name}`).style['transform'] = `translate(0, 0) translate(${xMovement.toFixed(0)}px, ${yMovement.toFixed(0)}px)`;
       }
-
-      let verticalMoveDirection = 1;
-      if (this.mouseY - windowHeight / 2 > 0) {
-        verticalMoveDirection = -1;
-      }
-
-      const xMovement = amountOfHorizontalMovement * horizontalMoveDirection * 100;
-      const yMovement = amountOfVerticalMovement * verticalMoveDirection * 100;
-
-      document.getElementById("target").style['transform'] = `translate(0, 0) translate(${xMovement.toFixed(0)}px, ${yMovement.toFixed(0)}px)`;
 
     },
     onMouseMove(evt) {
@@ -53,12 +80,28 @@ export default {
       this.mouseY = evt.screenY;
     }
   },
-  mounted() {
+  beforeMount() {
     // prepare the layers
-    this.layers.push({
-      img: "~@/assets/layer_pines.png"
-    })
+    this.layers.forEach(layer => {
+      const horizontalOffset = layer.horizontalDisplacement * 100 / 2;
+      const verticalOffset = layer.verticalDisplacement * 100 / 2;
+      this.stageLayers.push({
+        name: layer.name,
+        img: layer.image,
+        verticalDisplacement: layer.verticalDisplacement,
+        horizontalDisplacement: layer.horizontalDisplacement,
+        horizontalOffset: `-${horizontalOffset}%`,
+        verticalOffset: `-${verticalOffset}%`
+      });
+    });
+
     this.animateStage();
+  },
+  mounted() {
+    window.addEventListener("mousemove", this.onMouseMove);
+  },
+  beforeDestroy() {
+    window.removeEventListener("mousemove", this.onMouseMove);
   }
 }
 </script>
@@ -66,9 +109,6 @@ export default {
 <style lang="scss" scoped>
 #parallax {
   margin: 0 auto;
-  background-color: blueviolet;
-  width: 100vw;
-  height: 100vh;
   position: relative;
   overflow: hidden;
 
@@ -79,20 +119,17 @@ export default {
     background-color: white;
     border-radius: 4px;
     padding: 5px 10px;
+    z-index: 100;
+    display: none;
   }
 
   .layer {
     position: absolute;
-    left: 0 - 10%;
-    top: 0 + 10%;
-    bottom: 0 - 10%;
-    right: 0 + 10%;
     transform-origin: left bottom;
     will-change: transform;
-    transition: 250ms all linear;
-    background-image: url("~@/assets/layer_pines.png");
+    transition: 300ms all linear;
     background-repeat: no-repeat;
-    background-size: contain;
+    background-size: cover;
   }
 }
 </style>
